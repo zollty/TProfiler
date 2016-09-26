0850edd3dafde92532aacc50d6ad5265a0ca0b2b
zhoulifu <zlf369777684@gmail.com>
2016-09-26 15:31:59 +0800

    Simplify ProfClassAdapter

--- src/main/java/com/taobao/profile/instrument/ProfClassAdapter.java
+++ src/main/java/com/taobao/profile/instrument/ProfClassAdapter.java
@@ -13,7 +13,6 @@ import java.util.List;
 import org.objectweb.asm.ClassAdapter;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.FieldVisitor;
-import org.objectweb.asm.MethodAdapter;
 import org.objectweb.asm.MethodVisitor;
 
 import com.taobao.profile.Manager;
@@ -28,78 +27,58 @@ public class ProfClassAdapter extends ClassAdapter {
     /**
      * 类名
      */
-    private String mClassName;
+    private String className;
     /**
      * 文件名
      */
-    private String mFileName = null;
+    private String fileName = null;
+
     /**
-     * 字段对应方法列表
+     * Getter/setter method name cache.
      */
-    private List<String> fieldNameList = new ArrayList<String>();
+    private List<String> pojoMethodNames = new ArrayList<String>();
 
     /**
      * @param visitor
-     * @param theClass
+     * @param className
      */
-    public ProfClassAdapter(ClassVisitor visitor, String theClass) {
+    public ProfClassAdapter(ClassVisitor visitor, String className) {
         super(visitor);
-        this.mClassName = theClass;
+        this.className = className;
     }
 
-    /* (non-Javadoc)
-     * @see org.objectweb.asm.ClassAdapter#visit(int, int, java.lang.String, java.lang
-     * .String, java.lang.String, java.lang.String[])
-     */
-    public void visit(int version, int access, String name, String signature,
-            String superName, String[] interfaces) {
-        super.visit(version, access, name, signature, superName, interfaces);
-    }
-
-    /* (non-Javadoc)
-     * @see org.objectweb.asm.ClassAdapter#visitSource(java.lang.String, java.lang.String)
-     */
+    @Override
     public void visitSource(final String source, final String debug) {
         super.visitSource(source, debug);
-        mFileName = source;
+        fileName = source;
     }
 
-    /* (non-Javadoc)
-     * @see org.objectweb.asm.ClassAdapter#visitField(int, java.lang.String, java.lang
-     * .String, java.lang.String, java.lang.Object)
-     */
+    @Override
     public FieldVisitor visitField(int access, String name, String desc, String signature,
             Object value) {
         String up = name.substring(0, 1).toUpperCase() + name.substring(1, name.length());
-        String getFieldName = "get" + up;
-        String setFieldName = "set" + up;
-        String isFieldName = "is" + up;
-        fieldNameList.add(getFieldName);
-        fieldNameList.add(setFieldName);
-        fieldNameList.add(isFieldName);
+        String getter = "get" + up;
+        String setter = "set" + up;
+        String boolGetter = "is" + up;
+        pojoMethodNames.add(getter);
+        pojoMethodNames.add(setter);
+        pojoMethodNames.add(boolGetter);
 
         return super.visitField(access, name, desc, signature, value);
     }
 
-    /* (non-Javadoc)
-     * @see org.objectweb.asm.ClassAdapter#visitMethod(int, java.lang.String, java.lang
-     * .String, java.lang.String, java.lang.String[])
-     */
+    @Override
     public MethodVisitor visitMethod(int arg, String name, String descriptor,
             String signature, String[] exceptions) {
-        if (Manager.isIgnoreGetSetMethod()) {
-            if (fieldNameList.contains(name)) {
-                return super.visitMethod(arg, name, descriptor, signature, exceptions);
-            }
-        }
-        // 静态区域不注入
-        if ("<clinit>".equals(name)) {
-            return super.visitMethod(arg, name, descriptor, signature, exceptions);
-        }
-
         MethodVisitor mv = super.visitMethod(arg, name, descriptor, signature,
                                              exceptions);
-        MethodAdapter ma = new ProfMethodAdapter(mv, mFileName, mClassName, name);
-        return ma;
+
+        // Ignore getter/setter and the static initialization methods
+        if ((Manager.isIgnoreGetSetMethod() && pojoMethodNames.contains(name))
+            || ("<clinit>".equals(name))) {
+            return mv;
+        }
+
+        return new ProfMethodAdapter(mv, fileName, className, name);
     }
 }
