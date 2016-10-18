c73dcb623f3cfc00f5db96014e668df1df352121
zhoulifu <zlf369777684@gmail.com>
2016-10-18 12:14:08 +0800

    Add unit test

--- src/test/java/com/taobao/profile/config/ConfigurePropertiesTest.java
+++ src/test/java/com/taobao/profile/config/ConfigurePropertiesTest.java
@@ -2,18 +2,24 @@ package com.taobao.profile.config;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.taobao.profile.utils.VariableNotFoundException;
 
 public class ConfigurePropertiesTest {
+    private ConfigureProperties properties;
+
+    @Before
+    public void before(){
+        properties = new ConfigureProperties();
+    }
+
 
     @Test
     public void testConfigureProperties() {
-        Properties properties = new ConfigureProperties();
         properties.put("file.name", "tprofiler.log");
         properties.put("log.file.path", "${user.home}/${file.name}");
 
@@ -23,7 +29,6 @@ public class ConfigurePropertiesTest {
 
     @Test
     public void testConfigure() throws IOException {
-        Properties properties = new ConfigureProperties();
         InputStream in = getClass().getClassLoader().getResourceAsStream(
                 "profile.properties");
         properties.load(in);
@@ -41,10 +46,18 @@ public class ConfigurePropertiesTest {
     @SuppressWarnings("all")
     public void testReplaceVariables() throws VariableNotFoundException {
         String source = "${user.home}/logs/${user.language}/tprofiler.log";
-        ConfigureProperties properties = new ConfigureProperties();
         String str1 = properties.parsePlaceHolder(source);
         String str2 = System.getProperty("user.home") + "/logs/" + System.getProperty(
                 "user.language") + "/tprofiler.log";
         Assert.assertEquals(str1, str2);
+    }
+
+    @Test
+    public void testDefaultValue(){
+        Assert.assertEquals("default", properties.getProperty("foo", "default"));
+
+        properties.setProperty("foo", "${bar}");
+
+        Assert.assertEquals("default", properties.getProperty("foo", "default"));
     }
 }
